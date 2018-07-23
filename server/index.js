f0cf133b33ce60f649d08460717779b4152bcc08
訾绍飞 <zishaofei221@gmail.com>
2018-07-23 09:03:23 +0800

    搞个爬虫？

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-const Koa = require('koa');
-const app = new Koa();
+// const Koa = require('koa');
+// const app = new Koa();
+//
+//
+// app.use(async (ctx,next)=>{
+//     ctx.body='dashbord';
+// });
+//
+// app.listen(8899);
 
+const puppeteer = require('puppeteer-cn');
+(async () => {
+    const browser = await puppeteer.launch({
+        headless:false
+    });
+    const page = await browser.newPage();
+    await page.goto('https://y.qq.com');
+    await page.waitForSelector('body');
+    await page.screenshot({path: 'yqq.png',fullPage:true});
+    browser.close();
+})();
 
-app.use(async (ctx,next)=>{
-    ctx.body='dashbord';
-});
-
-app.listen(8899);
